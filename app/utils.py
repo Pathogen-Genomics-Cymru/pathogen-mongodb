710b05765c851b3e425cbd6337c06121b4bf47e6
Anna Price <36504362+annacprice@users.noreply.github.com>
2023-03-27 19:53:13 +0100

    update speciation comparison

--- app/utils.py
+++ app/utils.py
@@ -5,6 +5,7 @@ import csv
 import pandas as pd
 import numpy as np
 from collections import defaultdict
+import difflib
 
 def testconnection():
     """Tests connection to MongoDB, prints version if successful"""
@@ -23,6 +24,7 @@ def testconnection():
     except:
         print ("Cannot connect to MongoDB")
 
+def same_merge(x): return ','.join(x[x.notnull()].astype(str))
 
 def create_tb_csv():
     """Create output tb csv"""
@@ -42,18 +44,22 @@ def create_tb_csv():
     d = defaultdict(list)
     for item in collection_list:
         shortrunID = "_".join(item.split("_")[:2])
-        print (shortrunID)
         lenrunID = len(shortrunID)
         d[item[:lenrunID]].append(item)
     print(dict(d))
 
-    phw_speciation_fields = ["PHW Accession Number", "PHW Episode Number", "PHW Mykrobe Species ID", "PHW Kraken Species ID"]
-    lodestone_speciation_fields = ["PHW Accession Number", "PHW Episode Number", "lodestone top_hit.name"]
-    ukhsa_speciation_fields = ["PHW Accession Number", "PHW Episode Number", "MiseqOutput", "ukhsa Species"]
+    phw_speciation_fields = ["PHW Accession Number", "PHW Episode Number", "PHW Mykrobe Species ID", "PHW Mykrobe Species %", "PHW Kraken Species ID", "PHW Kraken species %"]
+    lodestone_speciation_fields = ["PHW Accession Number", "PHW Episode Number", "lodestone top_hit.name", "lodestone top_hit.phylogenetics.species", "lodestone error"]
+    ukhsa_speciation_fields = ["PHW Accession Number", "PHW Episode Number", "MiseqOutput", "ukhsa Species", "ukhsa Mykrobe_species_pct"]
 
     phw_resistance_fields = ["PHW Accession Number", "PHW Episode Number", "PHW LIMS Interim Rif Resistance: TEXT TO REPORT", "PHW LIMS Interim Izh resistance: TEXT TO REPORT"]
     lodestone_resistance_fields = ["PHW Accession Number", "PHW Episode Number", "lodestone data.EFFECTS.CAP", "lodestone data.EFFECTS.EMB", "lodestone data.EFFECTS.INH", "lodestone data.EFFECTS.LEV", "lodestone data.EFFECTS.MXF", "lodestone data.EFFECTS.PZA", "lodestone data.EFFECTS.RIF", "lodestone data.EFFECTS.STM"]
     ukhsa_resistance_fields = ["PHW Accession Number", "PHW Episode Number", "MiseqOutput", "ukhsa type_MOX", "ukhsa type_INH", "ukhsa type_SM", "ukhsa type_EMB", "ukhsa type_RIF", "ukhsa type_AK", "ukhsa type_CAP", "ukhsa type_CIP", "ukhsa type_KAN", "ukhsa type_OFX", "ukhsa type_PZA", "ukhsa type_QUI"]
+
+
+    mycoTaxMap = pd.read_csv('mycoTaxMap.csv')
+    old_spec = mycoTaxMap['name_old'].tolist()
+    new_spec = mycoTaxMap['name_new'].tolist()
 
     # speciation and resistance csv
     for key, value in d.items():
@@ -79,13 +85,7 @@ def create_tb_csv():
 
                 phw_species = phw_df.drop(columns=[col for col in phw_df if col not in phw_speciation_fields])
 
-                phw_csv_species = str(nested_val) + "_speciation.csv"
-                phw_species.to_csv(phw_csv_species)
-
                 phw_resistance = phw_df.drop(columns=[col for col in phw_df if col not in phw_resistance_fields])
-
-                phw_csv_resistance = str(nested_val) + "_resistance.csv"
-                phw_resistance.to_csv(phw_csv_resistance)
 
             if nested_val.endswith("_lodestone"):
 
@@ -102,13 +102,27 @@ def create_tb_csv():
 
                 lodestone_df['PHW Episode Number'] = lodestone_df['PHW Episode Number'].astype('object')
 
+                lodestone_df = lodestone_df.rename(columns = { i: "lodestone top_hit.phylogenetics.species" for i in lodestone_df.columns if i.startswith("lodestone top_hit.phylogenetics.species") } )
+
                 lodestone_species = lodestone_df.drop(columns=[col for col in lodestone_df if col not in lodestone_speciation_fields])
-                lodestone_csv_species = str(nested_val) + "_speciation.csv"
-                lodestone_species.to_csv(lodestone_csv_species)
+
+                lodestone_species = lodestone_species.groupby(level=0, axis=1).apply(lambda x: x.apply(same_merge, axis=1))
+
+                match = []
+                lode_spec = lodestone_species['lodestone top_hit.name'].tolist()
+
+                for elem in lode_spec:
+                    try:
+                        match_string = difflib.get_close_matches(str(elem), new_spec, n=1)[0]
+                        match_index = new_spec.index(match_string)
+                        old_match = old_spec[int(match_index)]
+                        match.append(' '.join(old_match.split()[:2]))
+                    except:
+                        match.append('')
+
+                lodestone_species['lodestone normalised species'] = match
 
                 lodestone_resistance = lodestone_df.drop(columns=[col for col in lodestone_df if col not in lodestone_resistance_fields])
-                lodestone_csv_resistance = str(nested_val) + "_resistance.csv"
-                lodestone_resistance.to_csv(lodestone_csv_resistance)
 
             if nested_val.endswith("_ukhsa"):
 
@@ -117,17 +131,28 @@ def create_tb_csv():
                 ukhsa_df = ukhsa_df.rename(columns={'ukhsa Accession': 'PHW Accession Number', 'ukhsa Episode': 'PHW Episode Number', 'ukhsa MiseqOutput' : 'MiseqOutput'})
 
                 ukhsa_species = ukhsa_df.drop(columns=[col for col in ukhsa_df if col not in ukhsa_speciation_fields])
-                ukhsa_csv_species = str(nested_val) + "_speciation.csv"
-                ukhsa_species.to_csv(ukhsa_csv_species)
+
+                match=[]
+                ukhsa_spec = ukhsa_species['ukhsa Species'].tolist()
+
+                for elem in ukhsa_spec:
+                    try:
+                        match.append(difflib.get_close_matches(elem, old_spec, n=1)[0])
+                    except:
+                        match.append('')
+
+                ukhsa_species['ukhsa normalised species'] = match
 
                 ukhsa_resistance = ukhsa_df.drop(columns=[col for col in ukhsa_df if col not in ukhsa_resistance_fields])
-                ukhsa_csv_resistance = str(nested_val) + "_resistance.csv"
-                ukhsa_resistance.to_csv(ukhsa_csv_resistance)
 
-        species_df = ukhsa_species.merge(phw_species, on=['PHW Accession Number', 'PHW Episode Number'], how='left').merge(lodestone_species,  on=['PHW Accession Number', 'PHW Episode Number'], how='left')
-        species_csv_name = str(key) + "_speciation.csv"
-        species_df.to_csv(species_csv_name)
+        try:
+            species_df = ukhsa_species.merge(phw_species, on=['PHW Accession Number', 'PHW Episode Number'], how='left').merge(lodestone_species,  on=['PHW Accession Number', 'PHW Episode Number'], how='left')
+            species_csv_name = str(key) + "_speciation.csv"
+            species_df.to_csv(species_csv_name)
 
-        resistance_df = ukhsa_resistance.merge(phw_resistance, on=['PHW Accession Number', 'PHW Episode Number'], how='left').merge(lodestone_resistance,  on=['PHW Accession Number', 'PHW Episode Number'], how='left')
-        resistance_csv_name = str(key) + "_resistance.csv"
-        resistance_df.to_csv(resistance_csv_name)
+            resistance_df = ukhsa_resistance.merge(phw_resistance, on=['PHW Accession Number', 'PHW Episode Number'], how='left').merge(lodestone_resistance,  on=['PHW Accession Number', 'PHW Episode Number'], how='left')
+            resistance_csv_name = str(key) + "_resistance.csv"
+            resistance_df.to_csv(resistance_csv_name)
+
+        except:
+            print(str(key) + ": Error creating final speciation and resistance csvs")
