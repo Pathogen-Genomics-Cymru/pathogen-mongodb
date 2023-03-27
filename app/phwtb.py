dfccca7b1defe7088cfa944b90f585d507da20eb
Anna Price <36504362+annacprice@users.noreply.github.com>
2023-03-27 19:52:29 +0100

    update csv reader for phw and ukhsa

--- app/phwtb.py
+++ app/phwtb.py
@@ -3,7 +3,7 @@ import csv
 import os
 from dotenv import load_dotenv
 import glob
-
+import pandas as pd
 
 def phwtb_import(inputDir):
 
@@ -30,6 +30,9 @@ def phwtb_import(inputDir):
     input_csv = shortrunID + "_interim.csv"
 
     header = ["Accession Number", "Episode Number", "Mykrobe Species ID", "Mykrobe Species %", "Mykrobe Median", "Mykrobe Status", "PHW Basepairs", "PHW QC status", "Kraken Species ID", "Kraken species %", "Kraken Status", "NOTES", "LIMS Interim Species: TEXT TO REPORT", "LIMS Interim Rif Resistance: TEXT TO REPORT", "LIMS Interim Izh resistance: TEXT TO REPORT", "COMMENTS TO INCLUDE IN LIMS", "COMMENTS TO PENGU AND WCM"]
+
+
+    header = pd.read_csv(os.path.join(inputDir, input_csv), index_col=False, nrows=0).columns.tolist()
 
     with open(os.path.join(inputDir, input_csv), 'r') as infile:
         reader = csv.DictReader(infile)
